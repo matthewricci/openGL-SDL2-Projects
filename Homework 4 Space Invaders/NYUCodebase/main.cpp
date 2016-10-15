249cb32165991231d925157e537c9bb5e2476938
mr4039 <mr4039@nyu.edu>
2016-10-15 11:46:32 -0400

    adding HW4 folder to repo

---
+++ Homework 4 Space Invaders/NYUCodebase/main.cpp
@@ -0,0 +1,201 @@
+/*
+NAME: Matthew Ricci
+CLASS: CS3113 Homework 3 - Pong Remake
+NOTES: Press spacebar to start the game! The ball is represented by a rotating star (taken from my HW2 animation).
+	   The left padde is controlled with W and S, the right paddle is controlled with the Up and Down arrows.
+*/
+
+#ifdef _WINDOWS
+#include <GL/glew.h>
+#endif
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include <SDL_image.h>
+#include <vector>
+#include "Matrix.h"
+#include "ShaderProgram.h"
+
+#ifdef _WINDOWS
+#define RESOURCE_FOLDER ""
+#else
+#define RESOURCE_FOLDER "NYUCodebase.app/Contents/Resources/"
+#endif
+
+SDL_Window* displayWindow;
+
+enum GameState {START_SCREEN, LEVEL_ONE};
+int state;
+
+//loads textures for building
+GLuint LoadTexture(const char *image_path){
+	SDL_Surface *testTexture = IMG_Load(image_path);
+	GLuint textureID;
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_2D, textureID);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, testTexture->w, testTexture->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, testTexture->pixels);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	SDL_FreeSurface(testTexture);
+	return textureID;
+}
+
+void drawText(ShaderProgram *program, int fontTexture, std::string text, float size, float spacing, Matrix *modelMatrix) {
+	float texture_size = 1.0 / 16.0f;
+	std::vector<float> vertexData;
+	std::vector<float> texCoordData;
+
+	for (int i = 0; i < text.size(); i++) {
+		float texture_x = (float)(((int)text[i]) % 16) / 16.0f;
+		float texture_y = (float)(((int)text[i]) / 16) / 16.0f;
+		vertexData.insert(vertexData.end(), {
+			((size + spacing) * i) + (-0.5f * size), 0.5f * size,
+			((size + spacing) * i) + (-0.5f * size), -0.5f * size,
+			((size + spacing) * i) + (0.5f * size), 0.5f * size,
+			((size + spacing) * i) + (0.5f * size), -0.5f * size,
+			((size + spacing) * i) + (0.5f * size), 0.5f * size,
+			((size + spacing) * i) + (-0.5f * size), -0.5f * size,
+		});
+		texCoordData.insert(texCoordData.end(), {
+			texture_x, texture_y,
+			texture_x, texture_y + texture_size,
+			texture_x + texture_size, texture_y,
+			texture_x + texture_size, texture_y + texture_size,
+			texture_x + texture_size, texture_y,
+			texture_x, texture_y + texture_size,
+		});
+	}
+	glUseProgram(program->programID);
+
+	glVertexAttribPointer(program->positionAttribute, 2, GL_FLOAT, false, 0, vertexData.data());
+	glEnableVertexAttribArray(program->positionAttribute);
+	glVertexAttribPointer(program->texCoordAttribute, 2, GL_FLOAT, false, 0, texCoordData.data());
+	glEnableVertexAttribArray(program->texCoordAttribute);
+	glBindTexture(GL_TEXTURE_2D, fontTexture);
+	program->setModelMatrix(*modelMatrix);
+	glDrawArrays(GL_TRIANGLES, 0, text.size() * 6);
+
+	glDisableVertexAttribArray(program->positionAttribute);
+	glDisableVertexAttribArray(program->texCoordAttribute);
+}
+
+void Render(){
+
+}
+
+
+int main(int argc, char *argv[])
+{
+	SDL_Init(SDL_INIT_VIDEO);
+	displayWindow = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 360, SDL_WINDOW_OPENGL);
+	SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
+	SDL_GL_MakeCurrent(displayWindow, context);
+#ifdef _WINDOWS
+	glewInit();
+#endif
+
+	SDL_Event event;
+	bool done = false;
+
+	//setting default color
+	glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+
+	//texture coordinates
+	glViewport(0, 0, 640, 360);
+	ShaderProgram program(RESOURCE_FOLDER"vertex_textured.glsl", RESOURCE_FOLDER"fragment_textured.glsl");
+	glUseProgram(program.programID);
+	GLuint background = LoadTexture("Space Background.png");
+	GLuint square = LoadTexture("sprites.png");
+	int font = LoadTexture("font.png");
+	Matrix projectionMatrix;
+	Matrix modelMatrix;
+	Matrix viewMatrix;
+
+	projectionMatrix.setOrthoProjection(-3.55, 3.55, -2.0f, 2.0f, -1.0f, 1.0f);
+
+	//for keeping time
+	float lastFrameTicks = 0.0f;
+
+	//for starting game
+	bool start = false;
+
+
+	//start-screen text
+	std::string first_line = "~ Bootleg Invaders ~";
+	std::string second_line = "Press space to begin the adventure!";
+
+
+	while (!done) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
+				done = true;
+			}
+			else if (event.type == SDL_KEYDOWN){
+				if (event.key.keysym.scancode == SDL_SCANCODE_SPACE){    //press space to start the ball
+					if (!start){
+						start = true;
+					}
+				}
+			}
+		}
+		const Uint8 *keys = SDL_GetKeyboardState(NULL);
+
+		//start game by pressing space
+		if (state == START_SCREEN && keys[SDL_SCANCODE_SPACE])
+			state = LEVEL_ONE;
+
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+		float ticks = (float)SDL_GetTicks() / 1000.0f;
+		std::cout << SDL_GetTicks() << std::endl;
+		float elapsed = ticks - lastFrameTicks;
+		lastFrameTicks = ticks;
+
+		float vertices[] = { -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5, };
+
+		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+		glEnableVertexAttribArray(program.positionAttribute);
+
+		float texCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
+		glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+		glEnableVertexAttribArray(program.texCoordAttribute);
+
+		//setting up matrixes
+		program.setModelMatrix(modelMatrix);
+		program.setProjectionMatrix(projectionMatrix);
+		program.setViewMatrix(viewMatrix);
+
+		//drawing the background image
+		glBindTexture(GL_TEXTURE_2D, background);
+		modelMatrix.identity();
+		modelMatrix.Scale(8.0f, 4.5f, 1.0f);
+		program.setModelMatrix(modelMatrix);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		//drawing the start screen text, if gamestate is set to it
+		if (state == START_SCREEN){
+			modelMatrix.identity();
+			modelMatrix.Translate(-2.7f, 0.5f, 0.0f);
+			drawText(&program, font, first_line, 0.25f, 0.03f, &modelMatrix);
+			modelMatrix.identity();
+			modelMatrix.Translate(-3.05f, 0.0f, 0.0f);
+			drawText(&program, font, second_line, 0.18f, 0.0f, &modelMatrix);
+		}
+		else {
+			modelMatrix.identity();
+		}
+
+
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+
+		glDisableVertexAttribArray(program.positionAttribute);
+		glDisableVertexAttribArray(program.texCoordAttribute);
+
+		SDL_GL_SwapWindow(displayWindow);
+
+	}
+	SDL_Quit();
+	return 0;
+}
