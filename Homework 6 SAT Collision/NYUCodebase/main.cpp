6cb656ce3c094f83089af0b8329913dc1bf006cb
mr4039 <mr4039@nyu.edu>
2016-12-07 21:48:30 -0500

    added labels to textures, SAT collision is slightly working but is not completely correct yet

--- Homework 6 SAT Collision/NYUCodebase/main.cpp
+++ Homework 6 SAT Collision/NYUCodebase/main.cpp
@@ -1,7 +1,9 @@
 /*
 NAME: Matthew Ricci
 CLASS: CS3113 Homework 6 - SAT Collision Demo
-NOTES: 
+NOTES: This demo has three objects, white1 white2 white3, all from the same sprite, that are being scaled/rotated/translated at different rates.
+The SAT algorithm checks collision for each set of edges for each object on each run of the Update() function.
+reversing the rotation during Update() makes things wonky
 */
 
 #ifdef _WINDOWS
@@ -28,24 +30,11 @@ using namespace std;
 #define RESOURCE_FOLDER "NYUCodebase.app/Contents/Resources/"
 #endif
 
-#define LEVEL_HEIGHT 23     //number of rows of tiles (y value)
-#define LEVEL_WIDTH 154     //number of columns of tiles (x value)
-#define SPRITE_COUNT_X 16   //number of columns in the tilesheet
-#define SPRITE_COUNT_Y 8    //number of rows in the tilesheet
-#define TILE_SIZE 0.2
-#define GRAVITY 28          //counteracts jumping velocity
-#define FRICTION 10
 // 60 FPS (1.0f/60.0f)
 #define FIXED_TIMESTEP 0.0166666f
 #define MAX_TIMESTEPS 6
-#define TRACKING false      //flag that sets viewMatrix player tracking
 
 SDL_Window* displayWindow;
-
-//global vars containing sounds used in-game, they are global vars so that class functions (e.g., Update) can access and play them
-Mix_Chunk *keySound;
-Mix_Chunk *jumpSound;
-Mix_Music *music;
 
 //class SheetSprite for mapping and storing sprites from a sheet
 class SheetSprite {
@@ -110,148 +99,9 @@ class Vector{
 public:
 	float x;
 	float y;
+	float z = 0.0f;
 
 };
-
-//converts two positions (e.g., player.x and player.y) into spots on the tile grid
-
-//class Entity to give all objects of the game a space to live in the program
-class Entity{
-public:
-	Entity(SheetSprite iSprite, float iWidth, float iHeight, float iX=0.0f, float iY=0.0f) : 
-		sprite(iSprite), width(iWidth), height(iHeight), x(iX), y(iY),
-	    velocity_x(0.0f), velocity_y(0.0f),
-	    collidedTop(false), collidedBottom(false), collidedLeft(false), collidedRight(false){}
-	void Draw(ShaderProgram *program){
-		sprite.Draw(program);
-	}
-
-	void ResetFlags(){
-		collidedTop = false;
-		collidedBottom = false;
-		collidedLeft = false;
-		collidedRight = false;
-	}
-
-	void Update(float elapsed){
-		x += velocity_x * elapsed;
-		if (x > 3.25f){
-			x = 3.23f;
-			velocity_x *= -1;
-		}
-		else if (x < -3.25f){
-			x = -3.23f;
-			velocity_x *= -1;
-		}
-		y += velocity_y * elapsed;
-		if (y > 1.98f){
-			y = 1.96f;
-			velocity_y *= -1;
-		}
-		else if (y < -1.98f){
-			y = -1.96f;
-			velocity_y *= -1;
-		}
-	}
-
-	SheetSprite sprite;
-	float x;
-	float y;
-	float width;
-	float height;
-	float velocity_x;
-	float velocity_y;
-	float rotation = 100.0f;
-	bool collidedTop;
-	bool collidedBottom;
-	bool collidedLeft;
-	bool collidedRight;
-};
-
-
-//detects collision via the basic box-to-box algorithm, return true if detected, false otherwise
-bool detectCollisionTwoEntities(const Entity *a, const Entity *b){
-	//if a's bottom is higher than b's top, no collision
-	if ((a->y - (a->height / 2)) > (b->y + (b->height / 2)))
-		return false;
-	//if a's top if lower than b's bottom, no collision
-	else if ((a->y + (a->height / 2)) < (b->y - (b->height / 2)))
-		return false;
-	//if a's left is larger than b's right, no collision
-	else if ((a->x - (a->width / 2)) > (b->x + (b->width / 2)))
-		return false;
-	//if a's right is smaller than b's left, no collision
-	else if ((a->x + (a->width / 2)) < (b->x - (b->width / 2)))
-		return false;
-	else
-		return true;
-}
-
-
-//draws a string to screen given a font sheet and a string
-void drawText(ShaderProgram *program, int fontTexture, std::string text, float size, float spacing, Matrix *modelMatrix) {
-	float texture_size = 1.0 / 16.0f;
-	std::vector<float> vertexData;
-	std::vector<float> texCoordData;
-
-	for (int i = 0; i < text.size(); i++) {
-		float texture_x = (float)(((int)text[i]) % 16) / 16.0f;
-		float texture_y = (float)(((int)text[i]) / 16) / 16.0f;
-		vertexData.insert(vertexData.end(), {
-			((size + spacing) * i) + (-0.5f * size), 0.5f * size,
-			((size + spacing) * i) + (-0.5f * size), -0.5f * size,
-			((size + spacing) * i) + (0.5f * size), 0.5f * size,
-			((size + spacing) * i) + (0.5f * size), -0.5f * size,
-			((size + spacing) * i) + (0.5f * size), 0.5f * size,
-			((size + spacing) * i) + (-0.5f * size), -0.5f * size,
-		});
-		texCoordData.insert(texCoordData.end(), {
-			texture_x, texture_y,
-			texture_x, texture_y + texture_size,
-			texture_x + texture_size, texture_y,
-			texture_x + texture_size, texture_y + texture_size,
-			texture_x + texture_size, texture_y,
-			texture_x, texture_y + texture_size,
-		});
-	}
-	glUseProgram(program->programID);
-
-	glVertexAttribPointer(program->positionAttribute, 2, GL_FLOAT, false, 0, vertexData.data());
-	glEnableVertexAttribArray(program->positionAttribute);
-	glVertexAttribPointer(program->texCoordAttribute, 2, GL_FLOAT, false, 0, texCoordData.data());
-	glEnableVertexAttribArray(program->texCoordAttribute);
-	glBindTexture(GL_TEXTURE_2D, fontTexture);
-	program->setModelMatrix(*modelMatrix);
-	glDrawArrays(GL_TRIANGLES, 0, text.size() * 6);
-
-	glDisableVertexAttribArray(program->positionAttribute);
-	glDisableVertexAttribArray(program->texCoordAttribute);
-}
-
-
-
-
-//updates the entire gamestate for all entities
-void Update(float timeElapsed, Entity *white1, Entity *white2, Entity *white3){
-	float fixedElapsed = timeElapsed;
-	if (fixedElapsed > FIXED_TIMESTEP * MAX_TIMESTEPS) {
-		fixedElapsed = FIXED_TIMESTEP * MAX_TIMESTEPS;
-	}
-	while (fixedElapsed >= FIXED_TIMESTEP) {
-		fixedElapsed -= FIXED_TIMESTEP;
-		white1->Update(FIXED_TIMESTEP);
-		white2->Update(FIXED_TIMESTEP);
-		white3->Update(FIXED_TIMESTEP);
-	}
-	white1->Update(fixedElapsed);
-	white2->Update(fixedElapsed);
-	white3->Update(fixedElapsed);
-}
-
-//renders entire gamestate (should be called AFTER all Update() calls
-void Render(Matrix *modelMatrix, Matrix *playerModelMatrix){
-
-}
 
 bool testSATSeparationForEdge(float edgeX, float edgeY, const std::vector<Vector> &points1, const std::vector<Vector> &points2) {
 	float normalX = -edgeY;
@@ -327,6 +177,303 @@ bool checkSATCollision(const std::vector<Vector> &e1Points, const std::vector<Ve
 	return true;
 }
 
+Vector convertToWorldCoords(Matrix transformations, Vector objCoords){
+	Vector worldCoords;
+	worldCoords.x = ( (transformations.m[0][0] * objCoords.x) + (transformations.m[0][1] * objCoords.y) + (transformations.m[0][2] * objCoords.z) );
+	worldCoords.y = ( (transformations.m[1][0] * objCoords.x) + (transformations.m[1][1] * objCoords.y) + (transformations.m[1][2] * objCoords.z) );
+	worldCoords.z = ( (transformations.m[2][0] * objCoords.x) + (transformations.m[2][1] * objCoords.y) + (transformations.m[2][2] * objCoords.z) );
+	return worldCoords;
+}
+
+//converts two positions (e.g., player.x and player.y) into spots on the tile grid
+
+//class Entity to give all objects of the game a space to live in the program
+class Entity{
+public:
+	Entity(SheetSprite iSprite, float iWidth, float iHeight, float iX=0.0f, float iY=0.0f) : 
+		sprite(iSprite), width(iWidth), height(iHeight), x(iX), y(iY),
+	    velocity_x(0.0f), velocity_y(0.0f),
+	    collidedTop(false), collidedBottom(false), collidedLeft(false), collidedRight(false)
+	{
+		Vector topLeftWorld, topRightWorld, bottomLeftWorld, bottomRightWorld;
+		epoints.push_back(topLeftWorld);
+		epoints.push_back(topRightWorld);
+		epoints.push_back(bottomLeftWorld);
+		epoints.push_back(bottomRightWorld);
+
+	}
+	void Draw(ShaderProgram *program){
+		sprite.Draw(program);
+	}
+
+	void ResetFlags(){
+		collidedTop = false;
+		collidedBottom = false;
+		collidedLeft = false;
+		collidedRight = false;
+	}
+
+	void Update(float elapsed, Matrix transformations){
+		x += velocity_x * elapsed;
+		if (x > 3.25f){
+			x = 3.23f;
+			velocity_x *= -1;
+		}
+		else if (x < -3.25f){
+			x = -3.23f;
+			velocity_x *= -1;
+		}
+		y += velocity_y * elapsed;
+		if (y > 1.98f){
+			y = 1.96f;
+			velocity_y *= -1;
+		}
+		else if (y < -1.98f){
+			y = -1.96f;
+			velocity_y *= -1;
+		}
+
+		//setting top-left corner
+		topLeftObj.x = x - (width / 2);
+		topLeftObj.y = y + (height / 2);
+		//setting top-right corner
+		topRightObj.x = x + (width / 2);
+		topRightObj.y = y + (height / 2);
+		//setting bottom-left corner
+		bottomLeftObj.x = x - (width / 2);
+		bottomLeftObj.y = y - (width / 2);
+		//setting bottom-right corner
+		bottomRightObj.x = x + (width / 2);
+		bottomRightObj.y = y - (width / 2);
+
+		//converting all points to world space
+		epoints[0] = convertToWorldCoords(transformations, topLeftObj);
+		epoints[1] = convertToWorldCoords(transformations, topRightObj);
+		epoints[2] = convertToWorldCoords(transformations, bottomLeftObj);
+		epoints[3] = convertToWorldCoords(transformations, bottomRightObj);
+
+		//adjusting x and y of objects for the translations
+		//x = epoints[0].x + (width / 2);
+		//y = epoints[0].y - (height / 2);
+	}
+
+	SheetSprite sprite;
+	float x;
+	float y;
+	float width;
+	float height;
+	float velocity_x;
+	float velocity_y;
+	float rotation = 100.0f;
+	bool collidedTop;
+	bool collidedBottom;
+	bool collidedLeft;
+	bool collidedRight;
+
+	std::vector<Vector> epoints;
+
+	//creating Vectors for Obj and World coords of each vertex
+	Vector topLeftObj, topRightObj, bottomLeftObj, bottomRightObj;
+};
+
+
+//updates the entire gamestate for all entities
+void Update(float timeElapsed, Entity *white1, Entity *white2, Entity *white3,
+			Matrix white1Matrix, Matrix white2Matrix, Matrix white3Matrix){
+	float fixedElapsed = timeElapsed;
+	if (fixedElapsed > FIXED_TIMESTEP * MAX_TIMESTEPS) {
+		fixedElapsed = FIXED_TIMESTEP * MAX_TIMESTEPS;
+	}
+	//dividing elapsed time into fixed updates
+	while (fixedElapsed >= FIXED_TIMESTEP) {
+		fixedElapsed -= FIXED_TIMESTEP;
+		white1->Update(FIXED_TIMESTEP, white1Matrix);
+		white2->Update(FIXED_TIMESTEP, white2Matrix);
+		white3->Update(FIXED_TIMESTEP, white3Matrix);
+		if (checkSATCollision(white1->epoints, white2->epoints)){
+			//if white1 is to the left of white 2
+			if (white1->x < white2->x){
+				white1->x -= 0.01f;
+				white2->x += 0.01f;
+			}
+			else {
+				white1->x += 0.01f;
+				white2->x -= 0.01f;
+			}
+			//if white1 is below white2
+			if (white1->y < white2->y){
+				white1->y -= 0.01f;
+				white2->y += 0.01f;
+			}
+			else {
+				white1->y += 0.01f;
+				white2->y -= 0.01f;
+			}
+			//reverse velocities
+			white1->velocity_x *= -1;
+			white1->velocity_y *= -1;
+			white2->velocity_x *= -1;
+			white2->velocity_y *= -1;
+			//reverse direction of rotatation
+			//white1->rotation *= -1;
+			//	white2->rotation *= -1;
+		}
+		if (checkSATCollision(white1->epoints, white3->epoints)){
+			//if white1 is to the left of white 3
+			if (white1->x < white3->x){
+				white1->x -= 0.01f;
+				white3->x += 0.01f;
+			}
+			else {
+				white1->x += 0.01f;
+				white3->x -= 0.01f;
+			}
+			//if white1 is below white3
+			if (white1->y < white3->y){
+				white1->y -= 0.01f;
+				white3->y += 0.01f;
+			}
+			else {
+				white1->y += 0.01f;
+				white3->y -= 0.01f;
+			}
+			//reverse velocities
+			white1->velocity_x *= -1;
+			white1->velocity_y *= -1;
+			white3->velocity_x *= -1;
+			white3->velocity_y *= -1;
+			//reverse direction of rotatation
+			//	white1->rotation *= -1;
+			//white3->rotation *= -1;
+		}
+		if (checkSATCollision(white2->epoints, white3->epoints)){
+			//if white2 is to the left of white3
+			if (white2->x < white3->x){
+				white2->x -= 0.01f;
+				white3->x += 0.01f;
+			}
+			else {
+				white2->x += 0.01f;
+				white3->x -= 0.01f;
+			}
+			//if white2 is below white3
+			if (white2->y < white3->y){
+				white2->y -= 0.01f;
+				white3->y += 0.01f;
+			}
+			else {
+				white2->y += 0.01f;
+				white3->y -= 0.01f;
+			}
+			//reverse velocities
+			white2->velocity_x *= -1;
+			white2->velocity_y *= -1;
+			white3->velocity_x *= -1;
+			white3->velocity_y *= -1;
+			//reverse direction of rotatation
+			//		white2->rotation *= -1;
+			//white3->rotation *= -1;
+		}
+	}
+
+	//one more round of updates to finish out left-over elapsed time
+	white1->Update(fixedElapsed, white1Matrix);
+	white2->Update(fixedElapsed, white2Matrix);
+	white3->Update(fixedElapsed, white3Matrix);
+
+	if (checkSATCollision(white1->epoints, white2->epoints)){
+		//if white1 is to the left of white 2
+		if (white1->x < white2->x){
+			white1->x -= 0.01f;
+			white2->x += 0.01f;
+		}
+		else {
+			white1->x += 0.01f;
+			white2->x -= 0.01f;
+		}
+		//if white1 is below white2
+		if (white1->y < white2->y){
+			white1->y -= 0.01f;
+			white2->y += 0.01f;
+		}
+		else {
+			white1->y += 0.01f;
+			white2->y -= 0.01f;
+		}
+		//reverse velocities
+		white1->velocity_x *= -1;
+		white1->velocity_y *= -1;
+		white2->velocity_x *= -1;
+		white2->velocity_y *= -1;
+		//reverse direction of rotatation
+		//white1->rotation *= -1;
+	//	white2->rotation *= -1;
+	}
+	if (checkSATCollision(white1->epoints, white3->epoints)){
+		//if white1 is to the left of white 3
+		if (white1->x < white3->x){
+			white1->x -= 0.01f;
+			white3->x += 0.01f;
+		}
+		else {
+			white1->x += 0.01f;
+			white3->x -= 0.01f;
+		}
+		//if white1 is below white3
+		if (white1->y < white3->y){
+			white1->y -= 0.01f;
+			white3->y += 0.01f;
+		}
+		else {
+			white1->y += 0.01f;
+			white3->y -= 0.01f;
+		}
+		//reverse velocities
+		white1->velocity_x *= -1;
+		white1->velocity_y *= -1;
+		white3->velocity_x *= -1;
+		white3->velocity_y *= -1;
+		//reverse direction of rotatation
+	//	white1->rotation *= -1;
+		//white3->rotation *= -1;
+	}
+	if (checkSATCollision(white2->epoints, white3->epoints)){
+		//if white2 is to the left of white3
+		if (white2->x < white3->x){
+			white2->x -= 0.01f;
+			white3->x += 0.01f;
+		}
+		else {
+			white2->x += 0.01f;
+			white3->x -= 0.01f;
+		}
+		//if white2 is below white3
+		if (white2->y < white3->y){
+			white2->y -= 0.01f;
+			white3->y += 0.01f;
+		}
+		else {
+			white2->y += 0.01f;
+			white3->y -= 0.01f;
+		}
+		//reverse velocities
+		white2->velocity_x *= -1;
+		white2->velocity_y *= -1;
+		white3->velocity_x *= -1;
+		white3->velocity_y *= -1;
+		//reverse direction of rotatation
+//		white2->rotation *= -1;
+		//white3->rotation *= -1;
+	}
+}
+
+//renders entire gamestate (should be called AFTER all Update() calls
+void Render(Matrix *modelMatrix, Matrix *playerModelMatrix){
+
+}
+
+
+
 
 int main(int argc, char *argv[])
 {
@@ -349,12 +496,19 @@ int main(int argc, char *argv[])
 	ShaderProgram program(RESOURCE_FOLDER"vertex_textured.glsl", RESOURCE_FOLDER"fragment_textured.glsl");
 	glUseProgram(program.programID);
 
+	GLuint testTexture = LoadTexture("player.png");
+	SheetSprite test(testTexture, 0.0f, 0.0f, 1.0f, 1.0f, 0.2f);
+	GLuint white1Texture = LoadTexture("white1.png");
+	GLuint white2Texture = LoadTexture("white2.png");
+	GLuint white3Texture = LoadTexture("white3.png");
 
-	GLuint whiteTexture = LoadTexture("white.png");
-	SheetSprite whiteSprite(whiteTexture, 0.0f, 0.0f, 40.0f / 40.0f, 40.0f / 40.0f, 0.8f);
-	Entity white1(whiteSprite, (whiteSprite.size*(whiteSprite.width/whiteSprite.height)-0.6f), whiteSprite.size-0.2f, 0.0f, 0.0f);
-	Entity white2(whiteSprite, (whiteSprite.size*(whiteSprite.width / whiteSprite.height) - 0.6f), whiteSprite.size - 0.0f, 0.0f);
-	Entity white3(whiteSprite, (whiteSprite.size*(whiteSprite.width / whiteSprite.height) - 0.6f), whiteSprite.size - 0.0f, 0.0f);
+
+	SheetSprite white1Sprite(white1Texture, 0.0f, 0.0f, 40.0f / 40.0f, 40.0f / 40.0f, 0.8f);
+	SheetSprite white2Sprite(white2Texture, 0.0f, 0.0f, 40.0f / 40.0f, 40.0f / 40.0f, 0.8f);
+	SheetSprite white3Sprite(white3Texture, 0.0f, 0.0f, 40.0f / 40.0f, 40.0f / 40.0f, 0.8f);
+	Entity white1(white1Sprite, (white1Sprite.size*(white1Sprite.width / white1Sprite.height)), white1Sprite.size, 0.0f, 0.0f);
+	Entity white2(white2Sprite, (white2Sprite.size*(white2Sprite.width / white2Sprite.height)), white2Sprite.size, 0.0f, 0.0f);
+	Entity white3(white3Sprite, (white3Sprite.size*(white3Sprite.width / white3Sprite.height)), white3Sprite.size, 0.0f, 0.0f);
 	white1.velocity_x = -1.0f;
 	white1.velocity_y = 1.0f;
 	white2.velocity_x = 2.0f;
@@ -404,22 +558,78 @@ int main(int argc, char *argv[])
 		program.setViewMatrix(viewMatrix);
 
 		//updates the entire game (all three white boxes) every frame
-		Update(elapsed, &white1, &white2, &white3);
+		Update(elapsed, &white1, &white2, &white3, white1ModelMatrix, white2ModelMatrix, white3ModelMatrix);
 
 		white1ModelMatrix.identity();
+		//white1ModelMatrix.Translate(white1.x, white1.y, 0.0f);
+		//white1ModelMatrix.Rotate(ticks * white1.rotation * (3.1415926 / 180.0));
+		white1ModelMatrix.Scale(2.0f, 0.3f, 1.0f);
 		white1ModelMatrix.Translate(white1.x, white1.y, 0.0f);
 		program.setModelMatrix(white1ModelMatrix);
 		white1.Draw(&program);
 		white2ModelMatrix.identity();
 		white2ModelMatrix.Translate(white2.x, white2.y, 0.0);
+		//white2ModelMatrix.Rotate(ticks * white2.rotation * (3.1415926 / 180.0));
 		program.setModelMatrix(white2ModelMatrix);
 		white2.Draw(&program);
 		white3ModelMatrix.identity();
 		white3ModelMatrix.Translate(white3.x, white3.y, 0.0f);						//follow TSR
-		white3ModelMatrix.Rotate(ticks * white3.rotation * (3.1415926 / 180.0));
+		//white3ModelMatrix.Rotate(white3.rotation * (3.1415926 / 180.0));
 		program.setModelMatrix(white3ModelMatrix);
 		white3.Draw(&program);
-		
+
+		for (size_t i = 0; i < white1.epoints.size(); i++){
+			modelMatrix.identity();
+			modelMatrix.Translate(white1.epoints[i].x, white1.epoints[i].y, white1.epoints[i].z);
+			program.setModelMatrix(modelMatrix);
+			test.Draw(&program);
+		}
+
+		for (size_t j = 0; j < white3.epoints.size(); j++){
+			modelMatrix.identity();
+			modelMatrix.Translate(white3.epoints[j].x, white3.epoints[j].y, white3.epoints[j].z);
+			program.setModelMatrix(modelMatrix);
+			test.Draw(&program);
+		}
+
+
+		//testing functions
+		//testing functions
+
+		//white1ModelMatrix.identity();
+		//white1ModelMatrix.Translate(-1.5f, 0.0f, 0.0f);
+		//white1.x = -1.5f;
+		//white1ModelMatrix.Rotate(45 * (3.1415926 / 180.0));
+		//program.setModelMatrix(white1ModelMatrix);
+		//white1.Draw(&program);
+		//
+		//white2ModelMatrix.identity();
+		//white2ModelMatrix.Translate(1.5f, 0.0f, 0.0f);
+		//white2.x = 1.5f;
+		//white2ModelMatrix.Rotate(-45 * (3.1415926 / 180.0));
+		//program.setModelMatrix(white2ModelMatrix);
+		//white2.Draw(&program);
+
+		//Vector white1Obj, white2Obj, white1World, white2World;
+		//white1Obj.x = white1.x + (white1.width / 2);
+		//white1Obj.y = white1.y - (white1.height / 2);
+		//white1Obj.z = 0;
+		//modelMatrix.identity();
+		//modelMatrix.Translate(white1Obj.x, white1Obj.y, white1Obj.z);
+		//program.setModelMatrix(modelMatrix);
+		//test.Draw(&program);
+		//white2Obj.x = white2.x - (white2.width / 2);
+		//white2Obj.y = white2.y - (white2.height / 2);
+		//white2Obj.z = 0;
+		//modelMatrix.identity();
+		//modelMatrix.Translate(white2Obj.x, white2Obj.y, white2Obj.z);
+		//program.setModelMatrix(modelMatrix);
+		//test.Draw(&program);
+		//white1World = convertToWorldCoords(white1ModelMatrix, white1Obj);
+		//white2World = convertToWorldCoords(white2ModelMatrix, white2Obj);
+
+		//end of testers
+
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
@@ -430,9 +640,7 @@ int main(int argc, char *argv[])
 		SDL_GL_SwapWindow(displayWindow);
 
 	}
-	Mix_FreeChunk(keySound);
-	Mix_FreeChunk(jumpSound);
-	Mix_FreeMusic(music);
+
 	SDL_Quit();
 	return 0;
 }
